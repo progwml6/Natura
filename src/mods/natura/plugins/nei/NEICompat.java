ffceda8e3fdcef726f93c16f518bbbf50dccfeb3
fuj1n <fuj1nman@hotmail.com>
2013-11-06 12:43:13 +1100

    Fixed a crash
    Fixed a crash if for some *weird* reason NEI was installed on a server (also, make it so that NEI crafting handlers are only registered on the client)

--- src/mods/natura/plugins/nei/NEICompat.java
+++ src/mods/natura/plugins/nei/NEICompat.java
@@ -5,11 +5,17 @@ import codechicken.nei.recipe.DefaultOverlayHandler;
 import codechicken.nei.api.API;
 import mods.natura.gui.WorkbenchGui;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.common.FMLCommonHandler;
+
+
 public class NEICompat {
 
 	public static void registerNEICompat(){
-		API.registerGuiOverlay(WorkbenchGui.class, "crafting");
-		API.registerGuiOverlayHandler(WorkbenchGui.class, new DefaultOverlayHandler(), "crafting");
+		if(FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT){
+			API.registerGuiOverlay(WorkbenchGui.class, "crafting");
+			API.registerGuiOverlayHandler(WorkbenchGui.class, new DefaultOverlayHandler(), "crafting");
+		}
 	}
 	
 }
