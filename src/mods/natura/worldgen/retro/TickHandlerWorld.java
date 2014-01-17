6e9099a8be116632cba61ebf8c9a11085e982c0e
Speiger Bär <moritzstehr@gmx.de>
2014-01-17 22:36:32 +0100

    Retrogen Fix (Who ever dirped around so Much)
    Now Here is a fix with the Retro gen. It does work. There were some
    really dirpy problems!

--- src/mods/natura/worldgen/retro/TickHandlerWorld.java
+++ src/mods/natura/worldgen/retro/TickHandlerWorld.java
@@ -1,56 +1,62 @@
 package mods.natura.worldgen.retro;
 
-    
-    import java.util.ArrayList;
-    import java.util.EnumSet;
-    import java.util.HashMap;
-    import java.util.Random;
-     
-    import net.minecraft.world.World;
-    import cpw.mods.fml.common.ITickHandler;
-    import cpw.mods.fml.common.TickType;
-     
-    public class TickHandlerWorld implements ITickHandler {
-     
-            public static TickHandlerWorld instance = new TickHandlerWorld();
-     
-            public static HashMap chunksToGen = new HashMap();
-     
-            @Override
-            public void tickStart(EnumSet<TickType> type, Object... tickData) {
-     
-            }
-     
-            @Override
-            public void tickEnd(EnumSet<TickType> type, Object... tickData) {
-     
-                    World world = (World) tickData[0];
-                    int dim = world.provider.dimensionId;
-                    ArrayList chunks = (ArrayList) chunksToGen.get(Integer.valueOf(dim));
-     
-                    if (chunks != null && chunks.size() > 0) {
-                            ChunkCoord c = (ChunkCoord) chunks.get(0);
-                            long worldSeed = world.getSeed();
-                            Random rand = new Random(worldSeed);
-                            long xSeed = rand.nextLong() >> 2 + 1L;
-                            long zSeed = rand.nextLong() >> 2 + 1L;
-                            rand.setSeed(xSeed * c.chunkX + zSeed * c.chunkZ ^ worldSeed);
-                            WorldHandler.instance.generateWorld(rand, c.chunkX, c.chunkZ, world, false);
-                            chunks.remove(0);
-                            chunksToGen.put(Integer.valueOf(dim), chunks);
-                    }
-            }
-     
-            @Override
-            public EnumSet<TickType> ticks() {
-     
-                    return EnumSet.of(TickType.WORLD);
-            }
-     
-            @Override
-            public String getLabel() {
-     
-                    return "NaturaWorld";
-            }
-     
-    }
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Random;
+
+import net.minecraft.world.World;
+import cpw.mods.fml.common.ITickHandler;
+import cpw.mods.fml.common.TickType;
+
+public class TickHandlerWorld implements ITickHandler
+{
+	
+	public static TickHandlerWorld instance = new TickHandlerWorld();
+	
+	//Defined is better so people know whats happening
+	public static HashMap<Integer, ArrayList<ChunkProsition>> chunksToGen = new HashMap<Integer, ArrayList<ChunkProsition>>();
+	
+	@Override
+	public void tickStart(EnumSet<TickType> type, Object... tickData)
+	{
+		
+	}
+	
+	@Override
+	public void tickEnd(EnumSet<TickType> type, Object... tickData)
+	{
+		
+		World world = (World) tickData[0];
+		int dim = world.provider.dimensionId;
+		ArrayList chunks = (ArrayList) chunksToGen.get(Integer.valueOf(dim));
+		
+		if (chunks != null && chunks.size() > 0)
+		{
+			ChunkProsition c = (ChunkProsition) chunks.get(0);
+			long worldSeed = world.getSeed();
+			Random rand = new Random(worldSeed);
+			long xSeed = rand.nextLong() >> 2 + 1L;
+			long zSeed = rand.nextLong() >> 2 + 1L;
+			rand.setSeed(xSeed * c.chunkX + zSeed * c.chunkZ ^ worldSeed);
+			WorldHandler.instance.generateWorld(rand, c.chunkX, c.chunkZ, world, true);
+			chunks.remove(0);
+			chunksToGen.put(Integer.valueOf(dim), chunks);
+		}
+	}
+	
+	@Override
+	public EnumSet<TickType> ticks()
+	{
+		
+		return EnumSet.of(TickType.WORLD);
+	}
+	
+	@Override
+	public String getLabel()
+	{
+		
+		return "NaturaWorld";
+	}
+	
+}
