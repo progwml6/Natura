7f95dab43c571e49f29dd35fde1e5e9825c0bab4
Zach <progwml6@gmail.com>
2014-01-16 19:54:05 -0500

    Update BoneBag.java

--- src/mods/natura/items/BoneBag.java
+++ src/mods/natura/items/BoneBag.java
@@ -14,6 +14,7 @@ import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.MinecraftForge;
