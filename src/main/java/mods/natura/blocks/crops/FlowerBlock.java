4d0ad58922b27ffb14fd2358cd7dd4cba48e068e
Progwml6 <progwml6@gmail.com>
2014-02-16 16:02:01 -0500

    prevent clientside only method from being called serverside

--- src/main/java/mods/natura/blocks/crops/FlowerBlock.java
+++ src/main/java/mods/natura/blocks/crops/FlowerBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.ObfuscationReflectionHelper;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -16,14 +17,18 @@ public class FlowerBlock extends BlockFlower
         super(0);
         setCreativeTab(CreativeTabs.tabDecorations);
         this.setStepSound(soundTypeGrass);
-        // TODO 1.7 pls no
-        ObfuscationReflectionHelper.setPrivateValue(BlockFlower.class, this, new IIcon[1], "field_149861_N");
+        // HINT: icons + serverside == bad
+        if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT)
+        {
+            // TODO 1.7 pls no
+            ObfuscationReflectionHelper.setPrivateValue(BlockFlower.class, this, new IIcon[1], "field_149861_N");
+        }
     }
-    
+
     @Override
-    public IIcon getIcon(IBlockAccess p_149673_1_, int p_149673_2_, int p_149673_3_, int p_149673_4_, int p_149673_5_)
+    public IIcon getIcon (IBlockAccess p_149673_1_, int p_149673_2_, int p_149673_3_, int p_149673_4_, int p_149673_5_)
     {
-    	return this.blockIcon;
+        return this.blockIcon;
     }
 
     @SideOnly(Side.CLIENT)
