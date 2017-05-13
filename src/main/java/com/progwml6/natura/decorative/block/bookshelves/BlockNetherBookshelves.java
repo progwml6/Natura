9975554334da8754b10770d32c6700086b42ee1f
BloodWorkXGaming <bloodworkxgaming@gmail.com>
2017-05-13 16:59:49 +0200

    Fixed Nether Booksshelf

--- src/main/java/com/progwml6/natura/decorative/block/bookshelves/BlockNetherBookshelves.java
+++ src/main/java/com/progwml6/natura/decorative/block/bookshelves/BlockNetherBookshelves.java
@@ -1,28 +1,25 @@
 package com.progwml6.natura.decorative.block.bookshelves;
 
-import java.util.Random;
-
-import javax.annotation.Nullable;
-
 import com.progwml6.natura.library.NaturaRegistry;
 import com.progwml6.natura.nether.block.planks.BlockNetherPlanks;
-
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import slimeknights.mantle.block.EnumBlock;
 
-public class BlockNetherBookshelves extends EnumBlock<BlockNetherPlanks.PlankType>
-{
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlockNetherBookshelves extends EnumBlock<BlockNetherPlanks.PlankType> {
     public static PropertyEnum<BlockNetherPlanks.PlankType> TYPE = PropertyEnum.create("type", BlockNetherPlanks.PlankType.class);
 
-    public BlockNetherBookshelves()
-    {
+    public BlockNetherBookshelves() {
         super(Material.WOOD, TYPE, BlockNetherPlanks.PlankType.class);
 
         this.setSoundType(SoundType.WOOD);
@@ -30,35 +27,18 @@ public class BlockNetherBookshelves extends EnumBlock<BlockNetherPlanks.PlankTyp
         this.setCreativeTab(NaturaRegistry.tabDecorative);
     }
 
-    /**
-     * Returns the quantity of items to drop on block destruction.
-     */
-    @Override
-    public int quantityDropped(Random random)
-    {
-        return 3;
-    }
 
-    /**
-     * Get the Item that this Block should drop when harvested.
-     */
     @Override
-    @Nullable
-    public Item getItemDropped(IBlockState state, Random rand, int fortune)
-    {
-        return Items.BOOK;
+    public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+        List<ItemStack> drops =  new ArrayList<>();
+        drops.add(new ItemStack(Items.BOOK, 3));
+        return drops;
     }
 
     @Override
-    public float getEnchantPowerBonus(World world, BlockPos pos)
-    {
+    public float getEnchantPowerBonus(World world, BlockPos pos) {
         return 1;
     }
 
-    @Override
-    @Deprecated
-    public boolean isFullCube(IBlockState state)
-    {
-        return false;
-    }
+
 }
