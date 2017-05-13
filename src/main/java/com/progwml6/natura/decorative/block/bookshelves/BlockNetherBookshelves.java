6cf19c497945848a6ef73d6510b7b63a18cfa3db
alexbegt <traincrazyb@aol.com>
2017-05-13 17:07:47 -0400

    Fix a bug I created with the previous commit, Pick blocking a bookshelf while in creative now gives you the correct bookshelf.

--- src/main/java/com/progwml6/natura/decorative/block/bookshelves/BlockNetherBookshelves.java
+++ src/main/java/com/progwml6/natura/decorative/block/bookshelves/BlockNetherBookshelves.java
@@ -2,6 +2,7 @@ package com.progwml6.natura.decorative.block.bookshelves;
 
 import java.util.Random;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import com.progwml6.natura.library.NaturaRegistry;
@@ -11,9 +12,12 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import slimeknights.mantle.block.EnumBlock;
 
@@ -28,6 +32,21 @@ public class BlockNetherBookshelves extends EnumBlock<BlockNetherPlanks.PlankTyp
         this.setSoundType(SoundType.WOOD);
         this.setHardness(1.5F);
         this.setCreativeTab(NaturaRegistry.tabDecorative);
+    }
+
+    /**
+     * Called when a user uses the creative pick block button on this block
+     *
+     * @param target The full target the player is looking at
+     * @return A ItemStack to add to the player's inventory, Null if nothing should be added.
+     */
+    @Nonnull
+    @Override
+    public ItemStack getPickBlock(@Nonnull IBlockState state, RayTraceResult target, @Nonnull World world, @Nonnull BlockPos pos, EntityPlayer player)
+    {
+        int meta = this.getMetaFromState(state);
+
+        return new ItemStack(Item.getItemFromBlock(this), 1, meta);
     }
 
     /**
