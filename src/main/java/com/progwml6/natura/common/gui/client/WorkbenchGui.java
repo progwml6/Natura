2aa6bd7d021ac7079308be2b3de7211c51838d92
alexbegt <traincrazyb@aol.com>
2017-08-29 14:08:04 -0400

    Remove recipe book support stop stop a crash regarding casting the vanilla container to the modded container. Fixes #392, #Fixes 393

--- src/main/java/com/progwml6/natura/common/gui/client/WorkbenchGui.java
+++ src/main/java/com/progwml6/natura/common/gui/client/WorkbenchGui.java
@@ -1,20 +1,11 @@
 package com.progwml6.natura.common.gui.client;
 
-import java.io.IOException;
-
 import com.progwml6.natura.common.gui.common.WorkbenchContainer;
 
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiButtonImage;
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.client.gui.recipebook.GuiRecipeBook;
-import net.minecraft.client.gui.recipebook.IRecipeShownListener;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.inventory.ClickType;
-import net.minecraft.inventory.ContainerWorkbench;
-import net.minecraft.inventory.Slot;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -22,15 +13,9 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class WorkbenchGui extends GuiContainer implements IRecipeShownListener
+public class WorkbenchGui extends GuiContainer
 {
     private static final ResourceLocation CRAFTING_TABLE_GUI_TEXTURES = new ResourceLocation("textures/gui/container/crafting_table.png");
-
-    private GuiButtonImage recipeButton;
-
-    private final GuiRecipeBook recipeBookGui;
-
-    private boolean widthTooNarrow;
 
     public WorkbenchGui(InventoryPlayer playerInv, World worldIn)
     {
@@ -40,56 +25,6 @@ public class WorkbenchGui extends GuiContainer implements IRecipeShownListener
     public WorkbenchGui(InventoryPlayer playerInv, World worldIn, BlockPos blockPosition)
     {
         super(new WorkbenchContainer(playerInv, worldIn, blockPosition));
-        this.recipeBookGui = new GuiRecipeBook();
-    }
-
-    /**
-     * Adds the buttons (and other controls) to the screen in question. Called when the GUI is displayed and when the
-     * window resizes, the buttonList is cleared beforehand.
-     */
-    @Override
-    public void initGui()
-    {
-        super.initGui();
-        this.widthTooNarrow = this.width < 379;
-        this.recipeBookGui.init(this.width, this.height, this.mc, this.widthTooNarrow, this.inventorySlots, ((WorkbenchContainer) this.inventorySlots).craftMatrix);
-        this.guiLeft = this.recipeBookGui.updateScreenPosition(this.widthTooNarrow, this.width, this.xSize);
-        this.recipeButton = new GuiButtonImage(10, this.guiLeft + 5, this.height / 2 - 49, 20, 18, 0, 168, 19, CRAFTING_TABLE_GUI_TEXTURES);
-        this.buttonList.add(this.recipeButton);
-    }
-
-    /**
-     * Called from the main game loop to update the screen.
-     */
-    @Override
-    public void updateScreen()
-    {
-        super.updateScreen();
-        this.recipeBookGui.tick();
-    }
-
-    /**
-     * Draws the screen and all the components in it.
-     */
-    @Override
-    public void drawScreen(int mouseX, int mouseY, float partialTicks)
-    {
-        this.drawDefaultBackground();
-
-        if (this.recipeBookGui.isVisible() && this.widthTooNarrow)
-        {
-            this.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
-            this.recipeBookGui.render(mouseX, mouseY, partialTicks);
-        }
-        else
-        {
-            this.recipeBookGui.render(mouseX, mouseY, partialTicks);
-            super.drawScreen(mouseX, mouseY, partialTicks);
-            this.recipeBookGui.renderGhostRecipe(this.guiLeft, this.guiTop, true, partialTicks);
-        }
-
-        this.renderHoveredToolTip(mouseX, mouseY);
-        this.recipeBookGui.renderTooltip(this.guiLeft, this.guiTop, mouseX, mouseY);
     }
 
     /**
@@ -110,94 +45,8 @@ public class WorkbenchGui extends GuiContainer implements IRecipeShownListener
     {
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         this.mc.getTextureManager().bindTexture(CRAFTING_TABLE_GUI_TEXTURES);
-        int i = this.guiLeft;
+        int i = (this.width - this.xSize) / 2;
         int j = (this.height - this.ySize) / 2;
         this.drawTexturedModalRect(i, j, 0, 0, this.xSize, this.ySize);
-    }
-
-    /**
-     * Test if the 2D point is in a rectangle (relative to the GUI). Args : rectX, rectY, rectWidth, rectHeight, pointX,
-     * pointY
-     */
-    @Override
-    protected boolean isPointInRegion(int rectX, int rectY, int rectWidth, int rectHeight, int pointX, int pointY)
-    {
-        return (!this.widthTooNarrow || !this.recipeBookGui.isVisible()) && super.isPointInRegion(rectX, rectY, rectWidth, rectHeight, pointX, pointY);
-    }
-
-    /**
-     * Called when the mouse is clicked. Args : mouseX, mouseY, clickedButton
-     */
-    @Override
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException
-    {
-        if (!this.recipeBookGui.mouseClicked(mouseX, mouseY, mouseButton))
-        {
-            if (!this.widthTooNarrow || !this.recipeBookGui.isVisible())
-            {
-                super.mouseClicked(mouseX, mouseY, mouseButton);
-            }
-        }
-    }
-
-    @Override
-    protected boolean hasClickedOutside(int p_193983_1_, int p_193983_2_, int p_193983_3_, int p_193983_4_)
-    {
-        boolean flag = p_193983_1_ < p_193983_3_ || p_193983_2_ < p_193983_4_ || p_193983_1_ >= p_193983_3_ + this.xSize || p_193983_2_ >= p_193983_4_ + this.ySize;
-        return this.recipeBookGui.hasClickedOutside(p_193983_1_, p_193983_2_, this.guiLeft, this.guiTop, this.xSize, this.ySize) && flag;
-    }
-
-    /**
-     * Called by the controls from the buttonList when activated. (Mouse pressed for buttons)
-     */
-    @Override
-    protected void actionPerformed(GuiButton button) throws IOException
-    {
-        if (button.id == 10)
-        {
-            this.recipeBookGui.initVisuals(this.widthTooNarrow, ((ContainerWorkbench) this.inventorySlots).craftMatrix);
-            this.recipeBookGui.toggleVisibility();
-            this.guiLeft = this.recipeBookGui.updateScreenPosition(this.widthTooNarrow, this.width, this.xSize);
-            this.recipeButton.setPosition(this.guiLeft + 5, this.height / 2 - 49);
-        }
-    }
-
-    /**
-     * Fired when a key is typed (except F11 which toggles full screen). This is the equivalent of
-     * KeyListener.keyTyped(KeyEvent e). Args : character (character on the key), keyCode (lwjgl Keyboard key code)
-     */
-    @Override
-    protected void keyTyped(char typedChar, int keyCode) throws IOException
-    {
-        if (!this.recipeBookGui.keyPressed(typedChar, keyCode))
-        {
-            super.keyTyped(typedChar, keyCode);
-        }
-    }
-
-    /**
-     * Called when the mouse is clicked over a slot or outside the gui.
-     */
-    @Override
-    protected void handleMouseClick(Slot slotIn, int slotId, int mouseButton, ClickType type)
-    {
-        super.handleMouseClick(slotIn, slotId, mouseButton, type);
-        this.recipeBookGui.slotClicked(slotIn);
-    }
-
-    @Override
-    public void recipesUpdated()
-    {
-        this.recipeBookGui.recipesUpdated();
-    }
-
-    /**
-     * Called when the screen is unloaded. Used to disable keyboard repeat events
-     */
-    @Override
-    public void onGuiClosed()
-    {
-        this.recipeBookGui.removed();
-        super.onGuiClosed();
     }
 }
