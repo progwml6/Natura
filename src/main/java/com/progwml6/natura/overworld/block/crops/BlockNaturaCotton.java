ef285788f2050a8689f974fc52048aa6e0fe7a4f
alexbegt <traincrazyb@aol.com>
2017-10-21 14:48:50 -0400

    Add back right click harvesting on cotton crops #400

--- src/main/java/com/progwml6/natura/overworld/block/crops/BlockNaturaCotton.java
+++ src/main/java/com/progwml6/natura/overworld/block/crops/BlockNaturaCotton.java
@@ -6,7 +6,15 @@ import com.progwml6.natura.shared.NaturaCommons;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.FakePlayer;
 
 public class BlockNaturaCotton extends BlockOverworldCrops
 {
@@ -45,6 +53,55 @@ public class BlockNaturaCotton extends BlockOverworldCrops
     protected ItemStack getCrop()
     {
         return NaturaCommons.cotton.copy();
+    }
+
+    @Override
+    public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn)
+    {
+        if (!worldIn.isRemote)
+        {
+            int age = worldIn.getBlockState(pos).getValue(AGE).intValue();
+
+            if (age == 4)
+            {
+                worldIn.setBlockState(pos, this.getDefaultState().withProperty(AGE, Integer.valueOf(2)), 3);
+
+                EntityItem entityitem = new EntityItem(worldIn, playerIn.posX, playerIn.posY - 1.0D, playerIn.posZ, NaturaCommons.cotton.copy());
+
+                worldIn.spawnEntity(entityitem);
+
+                if (!(playerIn instanceof FakePlayer))
+                {
+                    entityitem.onCollideWithPlayer(playerIn);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        int age = state.getValue(AGE).intValue();
+
+        if (age == 4)
+        {
+            if (worldIn.isRemote)
+                return true;
+
+            worldIn.setBlockState(pos, this.getDefaultState().withProperty(AGE, Integer.valueOf(2)), 3);
+
+            EntityItem entityitem = new EntityItem(worldIn, playerIn.posX, playerIn.posY - 1.0D, playerIn.posZ, NaturaCommons.cotton.copy());
+
+            worldIn.spawnEntity(entityitem);
+
+            if (!(playerIn instanceof FakePlayer))
+            {
+                entityitem.onCollideWithPlayer(playerIn);
+            }
+            return true;
+        }
+
+        return false;
     }
 
 }
