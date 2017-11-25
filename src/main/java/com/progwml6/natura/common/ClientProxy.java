1d187984604f64997bf30f8496c23e8d18b54b65
alexbegt <traincrazyb@aol.com>
2017-11-25 15:54:02 -0500

    Disabling modules will no longer crash. Closes #402, closes #408

--- src/main/java/com/progwml6/natura/common/ClientProxy.java
+++ src/main/java/com/progwml6/natura/common/ClientProxy.java
@@ -10,7 +10,7 @@ public class ClientProxy extends CommonProxy
 {
     protected void registerItemModelNatura(ItemStack item, String name)
     {
-        if (item != null && !StringUtils.isNullOrEmpty(name))
+        if (item.getItem() != null && !StringUtils.isNullOrEmpty(name))
         {
             ModelRegisterUtil.registerItemModel(item, Util.getResource(name));
         }
