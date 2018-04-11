aa2f3da629c82cbf3f2fe98515ad0c65e7592190
alexbegt <traincrazyb@aol.com>
2018-04-11 12:44:39 -0400

    Fix Topiary grasses not allowing flowers to be planted on them. Closes #440

--- src/main/java/com/progwml6/natura/overworld/block/grass/BlockColoredGrass.java
+++ src/main/java/com/progwml6/natura/overworld/block/grass/BlockColoredGrass.java
@@ -7,7 +7,13 @@ import com.progwml6.natura.library.NaturaRegistry;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.common.IPlantable;
 import slimeknights.mantle.block.EnumBlock;
 
 public class BlockColoredGrass extends EnumBlock<BlockColoredGrass.GrassType>
@@ -21,6 +27,19 @@ public class BlockColoredGrass extends EnumBlock<BlockColoredGrass.GrassType>
         this.setCreativeTab(NaturaRegistry.tabWorld);
         this.setHardness(0.6F);
         this.setSoundType(SoundType.PLANT);
+    }
+
+    @Override
+    public boolean canSustainPlant(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing direction, IPlantable plantable)
+    {
+        EnumPlantType plantType = plantable.getPlantType(world, pos.offset(direction));
+
+        if (plantType == EnumPlantType.Plains)
+        {
+            return true;
+        }
+
+        return super.canSustainPlant(state, world, pos, direction, plantable);
     }
 
     public enum GrassType implements IStringSerializable, EnumBlock.IEnumMeta
